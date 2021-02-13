d4367889fce17e909a03d45e9a8568e3b2478105
Veng Vannet <vannetvengpp@gmail.com>
2021-02-13 21:46:03 +0700

    test toast message with kotlin

--- app/src/main/java/com/vannet/thread/MainActivity.kt
+++ app/src/main/java/com/vannet/thread/MainActivity.kt
@@ -2,10 +2,16 @@ package com.vannet.thread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun threadTest(view: View) {
+        Toast.makeText(applicationContext,"hwsad",Toast.LENGTH_SHORT).show()
     }
 }
